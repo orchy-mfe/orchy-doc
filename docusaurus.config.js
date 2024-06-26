f5a70a89e9af06ad11bcac9dee20f9d8571e8edc
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-06-26 23:34:15 +0200

    chore: docusaurus@v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,10 @@
 // @ts-check
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
 const customizedSvgo = require('./customize-svgo');
+
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
